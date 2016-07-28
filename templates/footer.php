e1958e465b68c474180acec0b6af6829e47539b7
Ujjwal Bhardwaj <Ujjwal Bhardwaj>
2016-07-28 16:06:30 +0530

    Minor Changes

--- templates/footer.php
+++ templates/footer.php
@@ -4,65 +4,48 @@
 			
 				$footer_color_theme = "";
 				
-				if(strcmp($page, "/bvpcsi master/bvpcsi/public_html/index.php") == 0){
+				if(strcmp($page, "/index.php") == 0){
 				 $footer_color_theme = "footer-color-home";
 				 }
-				if(strcmp($page, "/bvpcsi master/bvpcsi/public_html/council.php") == 0){
+				if(strcmp($page, "/council.php") == 0){
 				 $footer_color_theme = "footer-color-council"; 
 				 }
-				if(strcmp($page, "/bvpcsi master/bvpcsi/public_html/gallery.php") == 0){
+				if(strcmp($page, "/gallery.php") == 0){
 				 $footer_color_theme = "footer-color-gallery";
 				 }
-				if(strcmp($page, "/bvpcsi master/bvpcsi/public_html/events.php") == 0){
+				if(strcmp($page, "/events.php") == 0){
 				 $footer_color_theme = "footer-color-events";
 				 }
-				if(strcmp($page, "/bvpcsi master/bvpcsi/public_html/login.php") == 0){
+				if(strcmp($page, "/login.php") == 0){
 				 $footer_color_theme = "footer-color-home";
 				 }
 				
 ?>
 <div class="container-fluid text-col <?php echo $footer_color_theme?>" style="">
 	<div class="col-md-6">
-	<div class="row">
-	<div class="col-md-6  text-box" style="color: #FFF">
-	<h5>EMAIL SIGN UP</h5>
-	</div>
-	</div>
-	<div class="row">
-	<div class="col-md-8 text-box" style="color: #FFF; ">
-	<h4 style="font-weight:100;">Get the latest news, events and schedules for workshops from the team.</h4>
-	</div>
-	</div>
-	<div class="row">
-	<div class="col-md-6 input-box" style="color: #FFF; ">
-	<input class="input_text_new" type="text" name="email" placeholder="Enter your Email Address"></input>
-	</div>
-	<div class="col-md-6 input-box" style="color: #FFF; ">
-	<input class="input_btn_new" type="button" name="signup" value="SIGN UP"/>
-	</div>
-	</div>
+		<div class="fb-page" data-width="500" data-height="300" data-href="https://www.facebook.com/bvcoecsi" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/bvcoecsi" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/bvcoecsi">BVP CSI</a></blockquote></div>
 	</div>
 	<div class="col-md-6">
-	<div class="row">
-	<div class="col-md-6  text-box" style="color: #FFF">
-	<h5>Connect Us</h5>
+		<div class="row">
+			<div class="col-md-6  text-box" style="color: #FFF">
+				<h5>Connect Us</h5>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 connect-box" style="color: #FFF">
+				<a href="https://www.facebook.com/bvcoecsi" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+				<a href="https://plus.google.com" target="_blank"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+				<a href="https://github.com/imujjwal96/bvpcsi/" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+			</div>
+		</div>
 	</div>
-	</div>
-	<div class="row">
-	<div class="col-md-12 connect-box" style="color: #FFF">
-	<a href="https://www.facebook.com"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-	<a href="https://www.plus.google.com"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-	<a href="https://www.github.com"><i class="fa fa-github" aria-hidden="true"></i></a>
-	</div>	
-	</div>
-	</div>
-	</div>
+</div>
 
 	<div class="container-fluid footer" style="background-color: #fff;">
 	<div class="row">
 	
-	<a href="https://www.bvpcsi.in"><img src="img/bvpcsilogo-black.png" style="height:10vh" /></a>
-	<span style="float: right;padding: 20px 0px;">Made with <i class="fa fa-heart" aria-hidden="true" style="color: #FF2D50"></i> by Team</span>
+	<a href="http://www.bvpcsi.in"><img src="img/bvpcsilogo-black.png" style="height:10vh" /></a>
+	<span style="float: right;padding: 20px 0px;">Made with <i class="fa fa-heart" aria-hidden="true" style="color: #FF2D50"></i> by <a href = "https://github.com/imujjwal96/bvpcsi/graphs/contributors" target="_blank">Team</a></span>
 	</div>
 	</div>
 
@@ -83,5 +66,13 @@
 			});
 		}); 
 	</script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=1662670077349303";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 </body>
 </html>
